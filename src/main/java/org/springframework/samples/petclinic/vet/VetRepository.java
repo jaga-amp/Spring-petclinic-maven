a32d70d92bd1f97d063bb70465f2b5ac30568f37
Administrator <Administrator@172.31.10.45>
2021-02-16 14:48:01 +0000

    Fix for remediation :InMemoryCaching-EHCache-EHCache - Cache Annotations

--- src/main/java/org/springframework/samples/petclinic/vet/VetRepository.java
+++ src/main/java/org/springframework/samples/petclinic/vet/VetRepository.java
@@ -16,7 +16,6 @@
 package org.springframework.samples.petclinic.vet;
 
 import java.util.Collection;
-
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.repository.Repository;
@@ -35,12 +34,15 @@ import org.springframework.transaction.annotation.Transactional;
  */
 public interface VetRepository extends Repository<Vet, Integer> {
 
-	/**
-	 * Retrieve all <code>Vet</code>s from the data store.
-	 * @return a <code>Collection</code> of <code>Vet</code>s
-	 */
-	@Transactional(readOnly = true)
-	@Cacheable("vets")
-	Collection<Vet> findAll() throws DataAccessException;
+    /**
+     *  Retrieve all <code>Vet</code>s from the data store.
+     *  @return a <code>Collection</code> of <code>Vet</code>s
+     */
+    @Transactional(readOnly = true)
+    @Cacheable("vets")
+    Collection<Vet> findAll() throws DataAccessException;
 
+    public void printOutput() {
+        System.out.println("Added new Method using FEGO Remediations");
+    }
 }
